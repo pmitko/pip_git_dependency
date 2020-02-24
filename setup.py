2eaf264984f9ed9ec216812521c45ff7358c2364
pmitko <pmitko@pixelfederation.com>
2020-02-24 22:55:57 +0100

    potential fix of missing source as dependency

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pip_git_dependency",
+    name="pip-git-dependency",
     packages=find_packages(),
     version='0.1.1',
     author='pmitko',
     author_email='pmitko@pixelfederation.com',
     url='https://github.com/pmitko',
     license='MIT',
-	zip_safe=False
+	zip_safe=False,
+	entry_points={
+        'console_scripts': ['pip_git_dependency=pipgitdependency:main'],
+    },
 )
