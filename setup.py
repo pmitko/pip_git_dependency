ebf610f1deb02e04d3fa71e4cb92eda36d049978
pmitko <pmitko@pixelfederation.com>
2020-02-25 12:27:17 +0100

    test install dependency without __init__ + renamings

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pip-git-dependency",
+    name="pip git dependency",
     packages=find_packages(),
     version='0.1.1',
     author='pmitko',
     author_email='pmitko@pixelfederation.com',
     url='https://github.com/pmitko',
-    license='MIT',
-	zip_safe=False
+    license='MIT'
 )
